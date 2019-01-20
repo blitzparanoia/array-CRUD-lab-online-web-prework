b30b840836f7fc21523ffc32b4c4228611c2dfda
Irene Saez <bluemilk44@gmail.com>
2019-01-20 01:55:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["cat"] << "arrays!"
+  array = ["cat"] 
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
