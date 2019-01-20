e80e871ade9fc0ced15dafbca0ee3c41511fc3d6
Irene Saez <bluemilk44@gmail.com>
2019-01-20 01:56:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  ["cat"].unshift "wow"
+  array = ["cat"]
+  array.unshift "wow"
 end
 
 def remove_element_from_end_of_array(array)
